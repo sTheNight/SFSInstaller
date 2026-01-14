fcc253c626a0a8d14c55d95c7a9f5f45cbd52fa4
sTheNight <sthenight@outlook.com>
2026-01-14 16:24:58 +0800

    默认勾选破解补丁安装选项

--- app/src/main/java/com/example/sfsinstaller/ui/states/ActionCheckedState.kt
+++ app/src/main/java/com/example/sfsinstaller/ui/states/ActionCheckedState.kt
@@ -1,6 +1,6 @@
 package com.example.sfsinstaller.ui.states
 
 data class ActionCheckedState(
-    val isModPatchSelected: Boolean = false,
+    val isModPatchSelected: Boolean = true,
     val isTranslationSelected: Boolean = false
 )
