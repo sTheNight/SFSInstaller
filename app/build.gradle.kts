5f08504f1e2a73d7f7272c8f8b78be401dd873c6
sTheNight <sthenight@outlook.com>
2026-01-29 21:10:39 +0800

    feat: 适配 1.6.00.12(773)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.StefMorojna.SpaceflightSimulator"
         minSdk = 29
         targetSdk = 36
-        versionCode = 770
-        versionName = "1.6.00.11-13"
+        versionCode = 773
+        versionName = "1.6.00.12-13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
