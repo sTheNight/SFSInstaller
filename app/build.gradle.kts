f9e7aa303ef1d17e258035c487fed07fa2c80f84
重铬酸钠 <sthenight@outlook.com>
2026-04-17 09:04:53 +0800

    feat: 适配 1.6.00.19(866)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-val gameVersion = "1.6.00.18"
+val gameVersion = "1.6.00.19"
 val installerVersion = "14"
 
 plugins {
@@ -22,7 +22,7 @@ android {
         applicationId = "com.StefMorojna.SpaceflightSimulator"
         minSdk = 29
         targetSdk = 36
-        versionCode = 853
+        versionCode = 866
         versionName = "$gameVersion-$installerVersion"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
